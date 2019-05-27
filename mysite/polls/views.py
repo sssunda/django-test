c262dcbf4599dd222ceeb0256e1005a1802ea997
sssunda <sundaeun93@gmail.com>
2019-05-27 23:04:38 +0900

    Complete to practice Django documents part 1 to 7

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,36 +4,21 @@ from django.urls import reverse
 from django.db.models import F
 from .models import Question, Choice
 from django.views import generic
-
-
-# Create your views here.
-# def index(request):
-#     latest_question_list= Question.objects.order_by('-pub_date')[:5]
-#     context = {
-#         'latest_question_list' : latest_question_list
-#     }
-#     return render(request,'polls/index.html',context)
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
